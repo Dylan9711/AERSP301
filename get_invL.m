160d7d8a6c8a8efde6aa0a57e4215775ee859ef0
Dylan9711 <43389001+Dylan9711@users.noreply.github.com>
2018-12-05 23:34:14 -0500

    Add files via upload

---
+++ get_invL.m
@@ -0,0 +1,34 @@
+
+
+
+function inv_L = get_invL(L,y,x)
+
+Z = zeros(x);
+B = eye(x);
+z_x = 1;
+%Move horizontal
+while z_x < x+1
+Z(1,z_x) = B(1,z_x);
+z_y = 1;
+
+%move vertical
+    while z_y < y+1
+    c_1 = 1;
+    sum = 0;
+    
+        while c_1 < z_y %Get the sume of elements after minus
+            sum = sum + L(z_y,z_x);
+            c_1 = c_1+1;
+        end
+
+    Z(z_y,z_x)=B(z_y,z_x)-sum;
+    z_y = z_y+1;
+    end
+    
+z_x = z_x+1;
+end
+
+inv_L = Z;
+end
+
+
---
+++ main.m
@@ -0,0 +1,43 @@
+% Input:NULL
+% Output:Inverse of matrix A
+
+clc
+clear
+
+A = [1 2 0 0 0 0 0;
+     1 3 1 0 0 0 0;
+     0 2 4 1 0 0 0;
+     0 0 1 3 1 0 0;
+     0 0 0 1 2 2 0;
+     0 0 0 0 1 1 1;
+     0 0 0 0 0 2 1]; %Original Matrix
+
+[y,x] = size(A);
+
+
+gamma = zeros(1,x);
+beta = zeros(1,x);
+alpha = zeros(1,x);
+
+gamma(1) = A(1,1);
+%Initialize 2 matrixes
+L = eye(x);
+U = zeros(x);
+
+counter = 2;
+while counter < x+1
+    alpha(counter) = A(counter,counter-1)/gamma(counter-1);
+    beta(counter-1) = A(counter-1,counter);
+    gamma(counter) = A(counter,counter)-alpha(counter)*beta(counter-1);
+    L(counter, counter-1) = alpha(counter);
+    U(counter-1,counter-1) = gamma(counter-1);
+    U(counter-1,counter) = beta(counter-1);
+    counter = counter+1;
+end
+
+inv_L = get_invL (L,y,x);
+
+inv_A = get_invA(U,Z,y,x);
+
+
+
